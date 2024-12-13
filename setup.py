51efbeb834c87f750bf84cbd7b527ebd772f1813
Thomas Buechner <thomas.buechner@cplace.com>
2024-12-13 12:22:48 +0100

    new version

--- setup.py
+++ setup.py
@@ -38,10 +38,8 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
 )
