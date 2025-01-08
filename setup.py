d34426ee4fa06bf3604893eeac4730830b88fa15
Thomas Buechner <thomas.buechner@cplace.com>
2025-01-08 10:33:21 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="claudesync",
-    version="0.1.0",
+    version="0.1.1",
     description="Synchronize local files with Claude AI projects",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
